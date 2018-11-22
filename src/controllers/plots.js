d136f692c87a5a18ff27474ef11160d245f0489c
Kevin Gilliard <kevin.gilliard8415@gmail.com>
2018-11-22 13:30:40 -0800

    Remove unnecessary info from tooltip

--- src/controllers/plots.js
+++ src/controllers/plots.js
@@ -104,7 +104,7 @@ const defaults = {
 
         const datasetLabel = data.datasets[item.datasetIndex].label || '';
         const value = data.datasets[item.datasetIndex].data[item.index];
-        let label = `${datasetLabel} ${typeof item.xLabel === 'string' ? item.xLabel : item.yLabel}`;
+        let label = `${datasetLabel}`;
         if (Array.isArray(data.datasets[item.datasetIndex].dataLabels) && data.datasets[item.datasetIndex].dataLabels.length > item.index) {
           label += ` ${data.datasets[item.datasetIndex].dataLabels[plots.currentItem]}`;
         }
