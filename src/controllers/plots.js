f7a1bd78761c40988dfc7d332fdbdf6fafbde896
Kevin Gilliard <kevin.gilliard8415@gmail.com>
2018-11-22 10:10:26 -0800

    Apply data label for each plots in tooltip text

--- src/controllers/plots.js
+++ src/controllers/plots.js
@@ -104,6 +104,10 @@ const defaults = {
         const datasetLabel = data.datasets[item.datasetIndex].label || '';
         const value = data.datasets[item.datasetIndex].data[item.index];
         let label = `${datasetLabel} ${typeof item.xLabel === 'string' ? item.xLabel : item.yLabel}`;
+        if (Array.isArray(data.datasets[item.datasetIndex].dataLabels) && data.datasets[item.datasetIndex].dataLabels.length > item.index) {
+          label += ` ${data.datasets[item.datasetIndex].dataLabels[item.index]}`;
+        }
+
         return `${label} ${value[currentItem]}`;
       }
     }
