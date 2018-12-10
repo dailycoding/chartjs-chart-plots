186ee2c6c6fd20bb3a57bb17bb317f2d659e55e5
Kevin Gilliard <kevin.gilliard8415@gmail.com>
2018-12-10 10:51:31 -0800

    Add babek package

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 // rollup.config.js
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
 
 export default {
   output: {
@@ -13,7 +14,22 @@ export default {
   },
   external: ['chart.js'],
   plugins: [
+    babel({
+      presets: [
+        [
+          "@babel/preset-env",
+          {
+            'targets': {
+              'browsers': ['ie 11']
+            }
+          }
+        ]
+      ],
+      babelrc: false,
+      comments: true,
+      runtimeHelpers: true,
+    }),
     resolve(),
-    commonjs()
+    commonjs(),
   ]
 };
